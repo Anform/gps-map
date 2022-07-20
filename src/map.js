60662e751077b65e832c7de9b634c1dc2c5757d0
aescoda <agustinescoda12345@gmail.com>
2022-07-20 15:09:09 -0400

    Implemented Random resturant markers nearby user location

--- src/map.js
+++ src/map.js
@@ -2,6 +2,7 @@ import { useState, useMemo, useCallback, useRef } from "react";
 import {
     GoogleMap,
     Marker,
+    Circle,
     MarkerClusterer,
   } from "@react-google-maps/api";
   import Places from "./places";
@@ -19,6 +20,10 @@ import {
     }), [])
 
     const onLoad = useCallback(map => (mapRef.current = map), []);
+    const stores = useMemo(function() {
+        if(location) return generateStores(location);
+    },[location])
+
 
     return <div className = "container">
         <div className = "controls">
@@ -26,18 +31,71 @@ import {
             <Places setLocation = {(position)=> {
                 setLocation(position);
                 mapRef.current.panTo(position);
-            }}></Places>
+            }}>
+            </Places>
         </div>
+
         <div className = "map">
             <GoogleMap 
                 zoom = {10} 
-                center = {center} 
+                center = {center}
                 mapContainerClassName = "map-container" 
                 options = {options}
                 onLoad = {onLoad}
             >
-                {location && <Marker position = {location}/>}
+            {location ? (
+                <>
+                    <Marker
+                        position={location}
+                        icon = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png">
+                    </Marker>
+                    {stores.map(store => (
+                        <Marker
+                            key = {store.lat}
+                            position = {store}
+                            >
+                            
+                        </Marker>
+                    ))}
+                    <Circle center={location} radius={1000} options={closeOptions} />
+                </>
+            ) :(
+                <>
+                </>
+            )}
             </GoogleMap>
         </div>
+
     </div>;
   }
+
+  const generateStores = (position) => {
+    const stores = []
+    const maxAmount = Math.random() * (50 - 10) + 10
+    for(let x = 0; x < maxAmount; x++)
+    {
+        const direction = Math.random() < 0.5 ? -100 : 100;
+        stores.push({
+            lat : position.lat + Math.random() / direction,
+            lng : position.lng + Math.random() / direction,
+        });
+    }
+    return stores; 
+}
+
+const defaultOptions = {
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+  };
+
+const closeOptions = {
+    ...defaultOptions,
+    zIndex: 3,
+    fillOpacity: 0.05,
+    strokeColor: "#8BC34A",
+    fillColor: "#8BC34A",
+  };
