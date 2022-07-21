302106cdeb4109033566061212a2e7abbfb4940a
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-07-21 13:20:35 -0400

    Updated Distance to display proper information

---
+++ src/distance.js
@@ -0,0 +1,12 @@
+export default function Distance({ leg }) {
+    if(!leg.distance || !leg.duration) return null;
+
+    return (
+        <div>
+            <p>
+                This resturant is <span className = "highlight">{leg.distance.text}</span> away 
+                from your current location. Estimated travel time : <span className = "highLight">{leg.duration.text}</span>
+            </p>
+        </div>
+    )
+}
